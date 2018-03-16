d9a3b3c6da87b12aa84588e13e1b74aeabe62379
Edgaru089 <sjs333@outlook.com>
2018-03-16 15:10:26 +0800

    Updated...

---
+++ Codes/20180316-PiGraphics.cpp
@@ -0,0 +1,23 @@
+/*
+DOCUMENT CODE "20180316-PiGraphics.cpp"
+CREATION DATE 2018-03-16
+SIGNATURE CODE_20180316_PIGRAPHICS
+TOPIC
+*/
+
+#include "Overall.hpp"
+
+//Check if this code file is enabled for testing.
+#ifdef CODE_20180316_PIGRAPHICS
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
+
+#endif
+
